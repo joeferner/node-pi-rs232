e82e4ee02b07fb7da1ebd670d586fdab8070008c
Joe Ferner <joe@fernsroth.com>
2017-03-19 21:54:51 -0400

    open serial port and write

--- index.js
+++ index.js
@@ -5,29 +5,105 @@ const bunyan = require('bunyan');
 
 const port = process.env.PORT || 8080;
 const LOGGER = createLogger();
+const receiveQueue = [];
 
-const app = express();
-app.use(bodyParser.json());
+function run() {
+    return findSerialPort()
+        .then(port => {
+            return openSerialPort(port);
+        })
+        .then((serialPort) => {
+            return startWebServer(serialPort);
+        });
+}
 
-app.post('/send', function(req, res) {
-    const value = req.body.value;
-    if (!body) {
-        return res.status(400).send('"value" is required');
-    }
-    res.send('OK');
-});
-
-app.use(express.static('public'));
-
-app.listen(port, function() {
-    console.log(`Listening on http://localhost:${port}`)
-});
-
-SerialPort.list(function (err, ports) {
-    ports.forEach(function(port) {
-        console.log(port);
+function writeToSerialPort(serialPort, value, options) {
+    LOGGER.info(`writing to serial port: ${value}`);
+    return new Promise((resolve, reject) => {
+        const beforeSendReceiveQueueSize = receiveQueue.length;
+        serialPort.write(value);
+        if (!options.waitForResponse) {
+            return resolve();
+        }
+        let timeout;
+        if (options.timeout) {
+            setTimeout(() => {
+                clearInterval(interval);
+                reject(new Error('Timeout waiting for response'));
+            }, options.timeout);
+        }
+        const interval = setInterval(() => {
+            if (receiveQueue.length !== beforeSendReceiveQueueSize) {
+                clearInterval(interval);
+                if (timeout) {
+                    clearTimeout(timeout);
+                }
+                resolve();
+            }
+        }, 10);
     });
-});
+}
+
+function startWebServer(serialPort) {
+    return new Promise((resolve, reject) => {
+        const app = express();
+        app.use(bodyParser.json());
+
+        app.post('/send', (req, res, next) => {
+            const value = req.body.value;
+            const waitForResponse = req.body.waitForResponse;
+            const timeout = req.body.timeout;
+            if (!value) {
+                return res.status(400).send('"value" is required');
+            }
+            writeToSerialPort(serialPort, value, {waitForResponse, timeout})
+                .then(() => {
+                    const response = JSON.stringify(receiveQueue);
+                    receiveQueue.length = 0;
+                    res.send(response);
+                })
+                .catch(err => {
+                    return next(err);
+                });
+        });
+
+        app.use(express.static('public'));
+
+        app.listen(port, function() {
+            LOGGER.info(`Listening on http://localhost:${port}`);
+            resolve();
+        });
+    });
+}
+
+function findSerialPort() {
+    return new Promise((resolve, reject) => {
+        SerialPort.list(function(err, ports) {
+            if (err) {
+                return reject(err);
+            }
+            if (ports.length === 0) {
+                return reject(new Error('Could not find valid serial ports'));
+            }
+            return resolve(ports[0]);
+        });
+    });
+}
+
+function openSerialPort(portInfo) {
+    return new Promise((resolve, reject) => {
+        LOGGER.info(`opening serial port ${portInfo.comName}`);
+        const serialPort = new SerialPort(portInfo.comName);
+        serialPort.on('open', () => {
+            LOGGER.info('port opened');
+            resolve(serialPort);
+        });
+        serialPort.on('data', (data) => {
+            LOGGER.info(`recv: ${data}`);
+            receiveQueue.push(data);
+        });
+    });
+}
 
 function createLogger() {
     return bunyan.createLogger({
@@ -44,3 +120,14 @@ function createLogger() {
         ]
     });
 }
+
+run()
+    .then(() => {
+        LOGGER.info('started');
+    })
+    .catch(err => {
+        if (err) {
+            LOGGER.error('could not start', err);
+            return process.exit(-1);
+        }
+    });
--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ export NODE_ENV=production
 
 cd /opt/node-pi-rs232
 echo "running" > /var/log/node-pi-rs232/node-pi-rs232.log
-/usr/bin/node index.js | ./node_modules/.bin/bunyan
+sudo /usr/bin/node index.js | ./node_modules/.bin/bunyan
 echo "finished $?" >> /var/log/node-pi-rs232/node-pi-rs232.log
