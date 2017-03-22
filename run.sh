ac51895dafa673caee4a2b155f14e32483461b6d
Joe Ferner <joe@fernsroth.com>
2017-03-22 14:55:55 -0400

    add forever to run script

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ export BAUD_RATE=9600
 
 cd /opt/node-pi-rs232
 echo "running" > /var/log/node-pi-rs232/node-pi-rs232.log
-/usr/bin/node index.js | ./node_modules/.bin/bunyan
+./node_modules/.bin/forever --minUptime 1000 --spinSleepTime 10000 index.js | ./node_modules/.bin/bunyan
 echo "finished $?" >> /var/log/node-pi-rs232/node-pi-rs232.log
