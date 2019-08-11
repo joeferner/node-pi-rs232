daca19cdabe63b0cc3f692730de32dfb56ffc68b
Joe Ferner <joe@fernsroth.com>
2019-08-11 11:17:40 -0400

    try and fix forever location

--- run.sh
+++ run.sh
@@ -4,10 +4,11 @@ export NODE_ENV=production
 export SERIAL_DEV=/dev/ttyUSB0
 export PORT=8080
 export BAUD_RATE=9600
-export LOG_FILE=/var/log/node-pi-rs232/node-pi-rs232.log
+export LOG_FILE=/tmp/node-pi-rs232.log
 export LOGSTASH_HOST=linux01
+export FOREVER_ROOT=/tmp
 
 cd /opt/node-pi-rs232
-echo "running" > /var/log/node-pi-rs232/node-pi-rs232.log
+echo "running" > /tmp/node-pi-rs232.log
 ./node_modules/.bin/forever --minUptime 1000 --spinSleepTime 10000 index.js | ./node_modules/.bin/bunyan
-echo "finished $?" >> /var/log/node-pi-rs232/node-pi-rs232.log
+echo "finished $?" >> /tmp/node-pi-rs232.log
